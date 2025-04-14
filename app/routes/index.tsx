3c5c68fb65a44807243002756c3949a872f83243
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-04-14 13:11:51 +0700

    #feat get location

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,63 @@
-import { Image, StyleSheet, Platform } from "react-native";
-import MapView from "react-native-maps";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Platform, PermissionsAndroid } from "react-native";
+import MapView, { Polyline, Region } from "react-native-maps";
 import { View } from "react-native";
+import * as Location from "expo-location";
 
 export default function HomeScreen() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      const { coords } = location;
+
+      if (coords) {
+        const { latitude, longitude } = coords;
+        console.log("Latitude:", latitude, "Longitude:", longitude);
+
+        let response = Location.reverseGeocodeAsync({
+          latitude,
+          longitude,
+        });
+        response.then((data) => {
+          console.log("Location data:", data);
+        });
+      } else {
+        setErrorMsg("Unable to retrieve location coordinates");
+      }
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      {location?.coords && (
+        <MapView
+          style={styles.map}
+          initialRegion={
+            location?.coords
+              ? {
+                  latitude: location.coords.latitude,
+                  longitude: location.coords.longitude,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }
+              : undefined
+          }
+          showsUserLocation={true} // Show the user's location on the map
+        />
+      )}
     </View>
   );
 }
