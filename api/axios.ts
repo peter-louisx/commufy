0b0e4b08e29e47f1af78944ea249152d05b57f1d
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-04-14 15:00:38 +0700

    #feat init axios

---
+++ api/axios.ts
@@ -0,0 +1,10 @@
+
+import Axios  from "axios";
+
+const axios = Axios.create({
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+  });
